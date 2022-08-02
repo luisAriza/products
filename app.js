5d0e990ffd66ee692052841429136cb4a430ad6c
Luis Ariza <luismanuelariza@hotmail.com>
2022-08-02 10:35:16 -0500

    Input's values

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+class Product {
+  constructor(name, price, year) {
+    this.name = name;
+    this.price = price;
+    this.year = year;
+  }
+}
 
+class UI {
+  addProduct(product) {
+    const productList = document.getElementById("product-list");
+    const element = document.createElement("div");
+  }
+  removeProduct() {}
+  showMessage() {}
+}
+
+document.getElementById("product-form").addEventListener("submit", (event) => {
+  const name = document.getElementById("name").value;
+  const price = document.getElementById("price").value;
+  const year = document.getElementById("year").value;
+
+  const product = new Product(name, price, year);
+
+  event.preventDefault();
+});
