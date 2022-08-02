08e5ed82015f511d497fc06bd86ff8c64a5add8a
Luis Ariza <luismanuelariza@hotmail.com>
2022-08-02 14:23:45 -0500

    Add and remove product function

--- app.js
+++ app.js
@@ -10,9 +10,38 @@ class UI {
   addProduct(product) {
     const productList = document.getElementById("product-list");
     const element = document.createElement("div");
+    element.className = "card text-center mb-3";
+    element.innerHTML = `
+			<div class="card-body">
+				<ul class="list-group">
+					<li class="list-group-item">
+						${product.name}
+					</li>
+					<li class="list-group-item">
+						$ ${product.price}
+					</li>
+					<li class="list-group-item mb-3">
+						<strong>Product year</strong>: ${product.year}
+					</li>
+				</ul>
+				<a href="/" class="btn btn-warning" name="remove">
+					Remove
+				</a>
+			</div>
+		`;
+    productList.appendChild(element);
+    this.resetForm();
   }
-  removeProduct() {}
-  showMessage() {}
+
+  resetForm() {
+    document.getElementById("product-form").reset();
+  }
+
+  removeProduct(element) {
+    element.name === "remove" && element.parentElement.parentElement.remove();
+  }
+
+  showMessage(msg, cssClass) {}
 }
 
 document.getElementById("product-form").addEventListener("submit", (event) => {
@@ -21,6 +50,16 @@ document.getElementById("product-form").addEventListener("submit", (event) => {
   const year = document.getElementById("year").value;
 
   const product = new Product(name, price, year);
+
+  const ui = new UI();
+  ui.addProduct(product);
+
+  event.preventDefault();
+});
+
+document.getElementById("product-list").addEventListener("click", (event) => {
+  const ui = new UI();
+  ui.removeProduct(event.target);
 
   event.preventDefault();
 });
